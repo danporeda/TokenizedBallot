389b4f048742143165e382a223cf234aa867d7cb
kavinm <30965883+kavinm@users.noreply.github.com>
2023-05-15 01:29:26 -0400

    added buy script

--- scripts/buyTokens.ts
+++ scripts/buyTokens.ts
@@ -1,1 +1,46 @@
-//  TODO: buy tokens with args: address, amount
+//  edit buyValue to change amount to be bought
+
+import { ethers } from "hardhat";
+import { MyERC20Token__factory, TokenSale__factory } from "../typechain-types";
+import * as dotenv from "dotenv";
+import { BigNumber } from "ethers";
+dotenv.config();
+
+const TOKEN_SALE_ADDRESS = "0x4d6670D962A5228442588f0c35CAC89EB9248f64";
+const TOKEN_ADDRESS = "0xd84744BC999c909828122779b4961FDc34f4E0d6";
+const main = async () => {
+  const provider = new ethers.providers.AlchemyProvider(
+    "maticmum",
+    process.env.ALCHEMY_API_KEY
+  );
+  const buyValue = ethers.utils.parseEther("0.01");
+  const pkey = process.env.PRIVATE_KEY;
+  const wallet = new ethers.Wallet(`${pkey}`);
+  const signer = wallet.connect(provider);
+
+  const tokenSaleFactory = new TokenSale__factory(signer);
+  const tokenSaleContract = tokenSaleFactory.attach(TOKEN_SALE_ADDRESS);
+  console.log(
+    `Contract factory created, attached to tokenSale at address ${tokenSaleContract.address}`
+  );
+
+  const tokenFactory = new MyERC20Token__factory(signer);
+  const tokenContract = tokenFactory.attach(TOKEN_ADDRESS);
+  console.log(
+    `Contract factory created, attached to ERC20Token at address ${tokenContract.address}`
+  );
+
+  //buying tokens
+  const buyTokensTx = await tokenSaleContract.buyTokens({ value: buyValue });
+  const tokenBalance = await tokenContract.balanceOf(signer.address);
+  console.log(
+    `Address ${signer.address} now has ${tokenBalance.div(
+      BigNumber.from("0xDE0B6B3A7640000")
+    )} Tokens `
+  );
+};
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,31 +1,41 @@
 import { ethers } from "hardhat";
 import { MyERC20Token__factory, TokenSale__factory } from "../typechain-types";
-import * as dotenv from 'dotenv';
+import * as dotenv from "dotenv";
 dotenv.config();
 
+const RATIO = 100;
 async function main() {
-    const provider = new ethers.providers.AlchemyProvider("maticmum", process.env.ALCHEMY_API_KEY);
-    const pkey = process.env.PRIVATE_KEY;
-    const wallet = new ethers.Wallet(`${pkey}`);
-    const signer = wallet.connect(provider);
+  const provider = new ethers.providers.AlchemyProvider(
+    "maticmum",
+    process.env.ALCHEMY_API_KEY
+  );
 
-    const tokenFactory = new MyERC20Token__factory(signer);
-    const tokenContract = await tokenFactory.deploy();
-    await tokenContract.deployed();
-    console.log(`token Contract deployed, address: ${tokenContract.address}`);
+  const pkey = process.env.PRIVATE_KEY;
 
-    const tokenSaleFactory = new TokenSale__factory(signer);
-    const tokenSaleContract = await tokenSaleFactory.deploy(10, tokenContract.address);
-    await tokenSaleContract.deployed();
-    console.log(`The tokensale contract address: ${tokenSaleContract.address}`);
-    const minterRole = await tokenContract.MINTER_ROLE();
-    const giveRoleTx = await tokenContract.grantRole(minterRole, tokenSaleContract.address);
-    await giveRoleTx.wait();
+  const wallet = new ethers.Wallet(`${pkey}`);
+  const signer = wallet.connect(provider);
 
+  const tokenFactory = new MyERC20Token__factory(signer);
+  const tokenContract = await tokenFactory.deploy();
+  await tokenContract.deployed();
+  console.log(`token Contract deployed, address: ${tokenContract.address}`);
 
+  const tokenSaleFactory = new TokenSale__factory(signer);
+  const tokenSaleContract = await tokenSaleFactory.deploy(
+    RATIO,
+    tokenContract.address
+  );
+  await tokenSaleContract.deployed();
+  console.log(`The tokensale contract address: ${tokenSaleContract.address}`);
+  const minterRole = await tokenContract.MINTER_ROLE();
+  const giveRoleTx = await tokenContract.grantRole(
+    minterRole,
+    tokenSaleContract.address
+  );
+  await giveRoleTx.wait();
 }
 
-main().catch((error) =>{
-    console.error(error);
-    process.exitCode = 1;
-})
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
